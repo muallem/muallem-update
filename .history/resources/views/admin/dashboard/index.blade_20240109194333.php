dcd7aca439e92ceead3f19daee7027bae0a1372b
muallem <muallem.id@gmail.com>
2024-01-09 19:43:58 +0700

    asha

--- .history/resources/views/admin/dashboard/index.blade_20240109194333.php
+++ .history/resources/views/admin/dashboard/index.blade_20240109194333.php
@@ -115,11 +115,11 @@
 
                         <!-- nav tab: menu list -->
                         <ul class="nav nav-tabs text-center mb-2" role="tablist">
-                            
+                            @if(\App\Helpers\AuthHelper::isAdmin())
                                 <li class="nav-item flex-fill"><a class="nav-link active" data-bs-toggle="tab" href="#admin_menu" role="tab">Admin</a></li>
-                            
+                            @else
                                 <li class="nav-item flex-fill"><a class="nav-link active" id="student_menu_nav_link" data-bs-toggle="tab" href="#student_menu" role="tab">Student</a></li>
-                            
+                            @endif
                         </ul>
                         @livewire('coba.index')
                     </div> 
