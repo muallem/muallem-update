d485f0113797d2ed69015a47ff39e029db6691aa
muallem <muallem.id@gmail.com>
2024-01-10 16:41:52 +0700

    asasd

--- resources/views/livewire/dashboard/category/filter.blade.php
+++ resources/views/livewire/dashboard/category/filter.blade.php
@@ -9,11 +9,11 @@
 
     {{-- Import Modal --}}
 
-    <div class="modal fade show" id="importModal" tabindex="-1" aria-labelledby="importModal" style="display: block;" aria-modal="true" role="dialog" wire:ignore.self>
+    <div class="modal fade" id="importModal" tabindex="-1" aria-labelledby="importModal" style="display: none;" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Import Data</h5>
+                    <h5 class="modal-title">Tambah Kategori</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 
--- resources/views/livewire/dashboard/materi/filter.blade.php
+++ resources/views/livewire/dashboard/materi/filter.blade.php
@@ -8,11 +8,11 @@
     </div>
 
     {{-- Import Modal --}}
-    <div class="modal fade show" id="importModal" tabindex="-1" aria-labelledby="importModal" style="display: block;" aria-modal="true" role="dialog" wire:ignore.self>
+    <div class="modal fade" id="importModal" tabindex="-1" aria-labelledby="importModal" style="display: none;" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Import Data</h5>
+                    <h5 class="modal-title">Import Materi</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 
