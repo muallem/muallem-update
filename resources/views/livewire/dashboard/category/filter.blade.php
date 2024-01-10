50b382ab08f4529e1c72d12cee39b907ff291261
muallem <muallem.id@gmail.com>
2024-01-10 16:39:13 +0700

    axva

--- resources/views/livewire/dashboard/category/filter.blade.php
+++ resources/views/livewire/dashboard/category/filter.blade.php
@@ -2,22 +2,21 @@
 <div class="row">
     <div class="col-sm-12 mb-3">
         <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#importModal">
-            <i class="fa fa-download"></i>
+            <i class="fa fa-plus"></i>
             Tambah Kategori
         </button>
     </div>
 
     {{-- Import Modal --}}
-    <div class="modal fade" id="importModal" data-backdrop="static" data-keyboard="false" tabindex="-1"
-        wire:ignore.self>
+
+    <div class="modal fade show" id="importModal" tabindex="-1" aria-labelledby="importModal" style="display: block;" aria-modal="true" role="dialog" wire:ignore.self>
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="importModalLabel">Tambah Kategori</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                    <h5 class="modal-title">Import Data</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
+                
                 <form wire:submit.prevent="store">
                     <div class="modal-body import_modal">
                         <div class="form-group mb-2">
