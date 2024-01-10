22d8248dee181108cedea202f8b3a6dcf9737e4b
muallem <muallem.id@gmail.com>
2024-01-10 16:25:33 +0700

    shg

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,7 @@ class RedirectIfAuthenticated
         foreach ($guards as $guard) {
             if (AuthHelper::isSessionToken()) {
                 if(AuthHelper::isAdmin()){
-                    return redirect()->route('admin.index');
+                    return redirect()->route('dashboard.index');
                 }
                 return redirect()->route('student.index');
             }
