816cac6fde21df7e45f0a6293d707e939322edd0
muallem <muallem.id@gmail.com>
2024-01-12 19:18:46 +0700

    asas

--- app/Livewire/Component/SidebarMenu.php
+++ app/Livewire/Component/SidebarMenu.php
@@ -10,33 +10,30 @@ use Illuminate\Support\Facades\DB;
 
 class SidebarMenu extends Component
 {
+    public $kata = 0;
     public $thesis_admin;
     public $thesis_student;
 
     public function mount()
     {
-        $this->getSidebar();
+        $this->getKata();
     }
 
-    public function getSidebar()
+    public function getKata()
     {
-        $this->thesis_admin = [];
-        $this->thesis_student = [];
-            $thesis = Judul::select('juduls.id', 'juduls.group', 'wpjs_users.user_login', DB::raw('COUNT(materi_feedback.id) as materi_count'))
+
+        $this->kata += 1;
+        $thesis = Judul::select('juduls.id', 'juduls.category_id', 'wpjs_users.user_login', DB::raw('COUNT(materi_feedback.id) as materi_count'))
             ->leftJoin('materi_feedback', function ($join) {
                 $join->on('juduls.student_id', '=', 'materi_feedback.student_id')
                     ->whereNull('materi_feedback.feedback');
             })
             ->leftJoin('wpjs_users', 'juduls.student_id', '=', 'wpjs_users.id')
-            ->groupBy('juduls.id', 'juduls.group', 'wpjs_users.user_login')
+            ->groupBy('juduls.id', 'juduls.category_id', 'wpjs_users.user_login')
             ->get()->toArray();
             $this->thesis_admin = $thesis;
         
-            $thesis_student = session()->get('user_id');
-            // $thesis_student = Judul::where('student_id', session()->get('user_id'))->first();
-            $this->thesis_student = $thesis_student;
-        
-        // $this->dispatchBrowserEvent('consoleLog', ['message' => $this->thesis]);
+            $this->thesis_student = Judul::where('student_id', session()->get('user_id'))->first();
     }
     public function render()
     {
