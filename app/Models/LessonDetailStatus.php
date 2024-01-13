d7f1565445415f3420b7acfb8f8926cd204fee7a
muallem <muallem.id@gmail.com>
2024-01-13 19:44:05 +0700

    ajdas

--- app/Models/LessonDetailStatus.php
+++ app/Models/LessonDetailStatus.php
@@ -3,12 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class LessonDetailStatus extends Model
 {   
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     const STATUS_SELESAI = 'SELESAI';
 
