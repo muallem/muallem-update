2180c0c7cb487852a67a86339a82497a2882a0c2
muallem <muallem.id@gmail.com>
2024-01-14 14:39:01 +0700

    adsad

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -27,6 +27,7 @@ class LessonController extends Controller
      */
     public function show($lesson_id, $judul_id)
     {
+        return Crypt::decryptString($lesson_id);
         return LessonDetail::select('lesson_details.*', 'lesson_detail_statuses.name')
         ->leftJoin('lesson_detail_statuses', 'lesson_details.id', '=', 'lesson_detail_statuses.lesson_detail_id')
         ->where('lesson_detail_statuses.student_id', 2)
