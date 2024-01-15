0f5a3738e33ccb74c405ce40298a2388f1b5e7fc
muallem <muallem.id@gmail.com>
2024-01-15 14:59:05 +0700

    sad

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -115,13 +115,13 @@
                         <!-- nav tab: menu list -->
                         <ul class="main-menu metismenu list-unstyled">
                             <li>
-                                <a href="{{ route('dashboard.materi') }}" class="text-decoration-none text-dark" aria-expanded="false">
+                                <a href="{{ route('superadmin.materi') }}" class="text-decoration-none text-dark" aria-expanded="false">
                                     <i class="fa fa-book"></i>
                                     <span>Materi</span> 
                                 </a>
                             </li>
                             <li>
-                                <a href="{{ route('dashboard.category') }}" class="text-decoration-none text-dark" aria-expanded="false">
+                                <a href="{{ route('superadmin.category') }}" class="text-decoration-none text-dark" aria-expanded="false">
                                     <i class="fa fa-th-large"></i>
                                     <span>Kategori</span> 
                                 </a>
