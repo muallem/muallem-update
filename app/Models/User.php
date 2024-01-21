9a2c14c5443c9840556c2fbd4b447eb21062ded8
muallem <muallem.id@gmail.com>
2024-01-21 17:09:09 +0700

    asdsa

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,10 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
     protected $table = 'wpjs_users';
     public $timestamps = false;
     /**
@@ -22,6 +19,7 @@ class User extends Authenticatable
         'name',
         'user_email',
         'user_pass',
+        'display_name',
     ];
 
     /**
@@ -40,6 +38,5 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'user_pass' => 'hashed',
     ];
 }
