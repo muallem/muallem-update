63a0ef86be904749b0fd5560fd780f953827593a
muallem <muallem.id@gmail.com>
2024-02-05 21:15:36 +0700

    res

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,6 +16,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $result = AuthHelper::login($request->user_email, $request->password);
+        return $result;
         if($result->getData()->message === 'Success' && $result->getData()->ok)
         {
             if(AuthHelper::isAdmin() )
